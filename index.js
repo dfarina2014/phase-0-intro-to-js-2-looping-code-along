6ff531510afc61f3775cd723142d2ff4868086df
daren farina <daren.farina@gmail.com>
2022-08-30 11:39:16 -0400

    commit tree 9a3e1ea56d2e1cde8131e6c2b9ff0738068dd0ef

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const name = ["Charlie", "Samip", "Ali"];
+
+function writeCards(name) {
+    let messages = []
+    for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+}
+
+function countDown(value){
+    while (value >= 0) {
+    console.log(value--)
+    }
+}
